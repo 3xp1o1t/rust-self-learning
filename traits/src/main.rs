2bbadd148514f55ebaca2c88a4c4d5883d4b25e1
3xp1o1t <jes.monti.503@gmail.com>
2024-01-03 21:54:19 -0600

    Add trait default implementation

--- traits/src/main.rs
+++ traits/src/main.rs
@@ -1,4 +1,4 @@
-use aggregator::{Summary, Tweet};
+use aggregator::{NewsArticle, Summary, Tweet};
 
 fn main() {
     let tweet = Tweet {
@@ -9,4 +9,14 @@ fn main() {
     };
 
     println!("A new tweet: {}", tweet.summarize());
+
+    // default implementation of a trait
+    let article = NewsArticle {
+        headline: String::from("Los pinguinos aman linux"),
+        location: String::from("kernel de linux"),
+        author: String::from("JMontiel"),
+        content: String::from("En el polo norte existe un pinguino llamado Linus que esta fumado."),
+    };
+
+    println!("New article available! {}", article.summarize());
 }
