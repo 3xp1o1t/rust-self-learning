cc3aff7c216c9e649b3f5f9786b638511fd00ec5
3xp1o1t <jes.monti.503@gmail.com>
2024-01-04 22:28:26 -0600

    Add struct lifetime

--- lifetimes/src/main.rs
+++ lifetimes/src/main.rs
@@ -1,8 +1,20 @@
+use std::fmt;
+
 fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
     if x.len() > y.len() {
         x
     } else {
         y
+    }
+}
+
+struct ImportanExcerpt<'z> {
+    part: &'z str,
+}
+
+impl<'z> fmt::Display for ImportanExcerpt<'z> {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "{}", self.part)
     }
 }
 
@@ -14,4 +26,12 @@ fn main() {
         let result = longest(string1.as_str(), string2.as_str());
         println!("The longest string is {}", result);
     }
+
+    let novel = String::from("Call me Ishmael. Some years ago...");
+    let first_sentence = novel.split('.').next().expect("Could not find a '.'");
+    let i = ImportanExcerpt {
+        part: first_sentence,
+    };
+
+    println!("First sentence: {}", i);
 }
