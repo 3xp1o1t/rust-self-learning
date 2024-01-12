3eaabda2c5c0e2247f7b1bddf60a45e7d2fad4ad
3xp1o1t <jes.monti.503@gmail.com>
2024-01-11 21:10:33 -0600

    Refactor minigrep using closures

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -8,13 +8,19 @@ pub struct Config {
 }
 
 impl Config {
-    pub fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("Not enough arguments.");
-        }
-        // &args[0] -> program name
-        let query = args[1].clone();
-        let file_path = args[2].clone();
+    pub fn build(mut args: impl Iterator<Item = String>) -> Result<Config, &'static str> {
+        // program file path
+        args.next();
+
+        let query = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a query string"),
+        };
+
+        let file_path = match args.next() {
+            Some(arg) => arg,
+            None => return Err("Didn't get a file path"),
+        };
 
         let ignore_case = env::var("IGNORE_CASE").is_ok();
 
@@ -43,28 +49,19 @@ pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
 }
 
 pub fn search<'a>(query: &str, file_content: &'a str) -> Vec<&'a str> {
-    let mut results = Vec::new();
-
-    for line in file_content.lines() {
-        if line.contains(query) {
-            results.push(line);
-        }
-    }
-
-    results
+    file_content
+        .lines()
+        .filter(|line| line.contains(query))
+        .collect()
 }
 
 pub fn search_case_insensitive<'a>(query: &str, file_content: &'a str) -> Vec<&'a str> {
     let query = query.to_lowercase(); // sombreado del arg query (shadowing)
-    let mut results = Vec::new();
 
-    for line in file_content.lines() {
-        if line.to_lowercase().contains(&query) {
-            results.push(line);
-        }
-    }
-
-    results
+    file_content
+        .lines()
+        .filter(|line| line.to_lowercase().contains(&query))
+        .collect()
 }
 
 #[cfg(test)]
