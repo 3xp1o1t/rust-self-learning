4f1f7037bd21d85520a541e5b045542dcd05d4d4
3xp1o1t <jes.monti.503@gmail.com>
2024-01-12 22:00:47 -0600

    Add pub use - re-export crates

--- documentation/src/main.rs
+++ documentation/src/main.rs
@@ -1,4 +1,12 @@
+use art::mix;
+use art::PrimaryColor;
+
 fn main() {
     println!("Generar docs");
     println!("cargo doc --open");
+
+    println!("Using pub use for creates.");
+    let red = PrimaryColor::Red;
+    let yellow = PrimaryColor::yellow;
+    mix(red, yellow);
 }
