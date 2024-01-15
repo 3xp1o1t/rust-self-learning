b0f8c149fc365a2bd3e822067e2739da36386c0a
3xp1o1t <jes.monti.503@gmail.com>
2024-01-15 15:30:43 -0600

    Add smart pointers drop & mem drop

--- smart-pointers/src/main.rs
+++ smart-pointers/src/main.rs
@@ -1,4 +1,5 @@
-use std::ops::Deref;
+use std::mem::drop;
+use std::ops::{Deref, Drop};
 
 struct MyBox<T>(T);
 
@@ -16,6 +17,16 @@ impl<T> Deref for MyBox<T> {
     }
 }
 
+struct CustomSmartPointer {
+    data: String,
+}
+
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
+    }
+}
+
 fn main() {
     let x = 5;
     let y = MyBox::new(x);
@@ -24,7 +35,31 @@ fn main() {
     assert_eq!(5, *y);
 
     let gretting = MyBox::new(String::from("Rust"));
-    hello(&gretting)
+    hello(&gretting);
+
+    println!("Trait drop examples");
+
+    let _c = CustomSmartPointer {
+        data: String::from("My Stuff"),
+    };
+
+    let _d = CustomSmartPointer {
+        data: String::from("Other Stuff"),
+    };
+
+    println!("CustomSmartPointer created.");
+
+    println!("CustomSmartPointer dropped using mem drop.");
+
+    let _m = CustomSmartPointer {
+        data: String::from("Mem drop"),
+    };
+
+    println!("CustomSmartPointer MEM DROP version created.");
+
+    drop(_m);
+
+    println!("CustomSmartPointer dropped before the end of main.");
 }
 
 // coercion deref
