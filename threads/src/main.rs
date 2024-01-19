9f0ac997a6d2e20e4d158ab7d835733ba56d8ff1
3xp1o1t <jes.monti.503@gmail.com>
2024-01-18 21:19:32 -0600

    Add threads handle join

--- threads/src/main.rs
+++ threads/src/main.rs
@@ -2,7 +2,7 @@ use std::thread;
 use std::time::Duration;
 
 mod basic_thread_example {
-    use std::thread;
+    use std::thread::{self, JoinHandle};
     use std::time::Duration;
 
     pub fn spawning_thread() {
@@ -17,10 +17,31 @@ mod basic_thread_example {
 }
 
 fn main() {
-    basic_thread_example::spawning_thread();
+    // Simple thread spawn
+    // basic_thread_example::spawning_thread();
+    //
+    // for i in 1..5 {
+    //     println!("Hi number {} from the MAIN thread!", i);
+    //     thread::sleep(Duration::from_millis(1));
+    // }
+
+    // Adding join handler to warranty full thread execution
+    // main thread finish, but handle thread keep running until finish
+
+    let handle = thread::spawn(|| {
+        for i in 1..10 {
+            println!("Hi number {} from the spawned thread!", i);
+            thread::sleep(Duration::from_millis(1));
+        }
+    });
+
+    // If we want that handle finish first, then execute main thread.
+    // handle.join().unwrap();
 
     for i in 1..5 {
         println!("Hi number {} from the MAIN thread!", i);
         thread::sleep(Duration::from_millis(1));
     }
+
+    handle.join().unwrap();
 }
