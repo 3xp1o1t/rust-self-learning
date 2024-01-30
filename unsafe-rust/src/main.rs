ebe8a119807e392b652c329fb01692b268dd1d01
3xp1o1t <jes.monti.503@gmail.com>
2024-01-29 21:42:08 -0600

    Add unsafe rust example

---
+++ unsafe-rust/src/main.rs
@@ -0,0 +1,17 @@
+unsafe fn swap(a: *mut u8, b: *mut u8) {
+    let temp = *a;
+    *a = *b;
+    *b = temp;
+}
+
+fn main() {
+    let mut a = 42;
+    let mut b = 12;
+
+    // Una funcion unsafe solo puede ser llamada dentro de un bloque unsafe.
+    unsafe {
+        swap(&mut a, &mut b);
+    }
+
+    println!("a = {}, b = {}", a, b);
+}
