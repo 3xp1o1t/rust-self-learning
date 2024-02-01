965d3053c45f6745f586116ca79e4736a667e374
3xp1o1t <jes.monti.503@gmail.com>
2024-01-31 18:10:27 -0600

    Add operator overload p2

--- operator-overload/src/main.rs
+++ operator-overload/src/main.rs
@@ -18,9 +18,23 @@ impl Add for Point {
     }
 }
 
+#[derive(Debug)]
+struct Millimeters(u32);
+struct Meters(u32);
+
+impl Add<Meters> for Millimeters {
+    type Output = Millimeters;
+    fn add(self, other: Meters) -> Millimeters {
+        Millimeters(self.0 + (other.0 * 1000))
+    }
+}
 fn main() {
     assert_eq!(
         Point { x: 1, y: 0 } + Point { x: 2, y: 3 },
         Point { x: 3, y: 3 }
     );
+    let mm = Millimeters(1);
+    let m = Meters(1);
+    let mm = mm + m;
+    println!("{:?}", mm);
 }
