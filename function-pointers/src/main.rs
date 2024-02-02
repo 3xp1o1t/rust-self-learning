7b64155ec87cbbb713c92172f5733992a1e5cd45
3xp1o1t <jes.monti.503@gmail.com>
2024-02-01 22:14:49 -0600

    Add return closure example

--- function-pointers/src/main.rs
+++ function-pointers/src/main.rs
@@ -7,7 +7,16 @@ fn do_twice(function: fn(i32) -> i32, value: i32) -> i32 {
     function(value) + function(value)
 }
 
+// retornar un closure
+fn returns_closure() -> Box<dyn Fn(i32) -> i32> {
+    Box::new(|x| x + 1)
+}
+
 fn main() {
     let add_one_twice = do_twice(add_one, 10);
     println!("{}", add_one_twice);
+
+    // return closure
+    let closure = returns_closure();
+    println!("{}", closure(10));
 }
